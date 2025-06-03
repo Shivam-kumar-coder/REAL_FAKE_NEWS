e381c08ab7e8d0a45b8402cc2b7d3fe01d51d21d
coder-shivam <innocentboy9472@gmail.com>
2025-06-03 11:40:42 +0530

    Update fake_real.py

--- fake_real.py
+++ fake_real.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn.feature_extraction.text import TfidfVectorizer
 from tensorflow.keras.models import load_model
 
 from tensorflow.keras.preprocessing.text import Tokenizer
